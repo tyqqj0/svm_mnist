ad6ccfde3c58801083354e1408aa9216b029bf07
tyqqj0 <tyqqj0@163.com>
2023-03-15 20:05:34 +0800

    完成代码整理

--- main.py
+++ main.py
@@ -16,21 +16,26 @@ import training.train as train
 import training.val as val
 import model.svm as svm
 
-# 处理标签
-
+#####################################################
+# TODO: 自定义训练集大小
+# 推荐实际使用1000就够，60000训练时间接近十分钟
+#####################################################
+args = {'train_size': 60000  # 训练集大小
+        }
+#####################################################
 
 if __name__ == '__main__':
     # 读取数据
-    (x_train, t_train), (x_test, t_test) = load_mnist('D:\Data\mnist')
+    (x_train, t_train), (x_test, t_test) = load_mnist('D:\Data\mnist')  # 读取数据
 
     # 生成模型
-    model = svm.svm_model()  #
+    model = svm.svm_model()  # 生成模型
 
     # 训练
-    clf = train.train_svm(model, x_train, t_train, train_size=1000)  # 很慢不要开太大
+    clf = train.train_svm(model, x_train, t_train, train_size=args['train_size'])  # 很慢不要开太大
 
     # 验证
-    # val.val(clf, x_test, t_test)
+    val.val(clf, x_test, t_test)
 
     # 画板测试
     c = True
